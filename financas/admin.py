065d73efb67b7823010f6004f75c8c7610e4da99
RobertoGine <robertogine@hotmail.com>
2025-12-04 14:21:05 -0300

    alteraçao no setting.py

--- financas/admin.py
+++ financas/admin.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 admin.site.register(User)
 
-@admin.register(Transacao)
+'''@admin.register(Transacao)
 class TransacaoAdmin(admin.ModelAdmin):
     list_display = ('usuario','tipo','categoria','valor','data')
     list_filter = ('tipo','categoria','data')
+'''
