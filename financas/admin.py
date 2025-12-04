cbbd4a0d7cfeff737170ca664600a75dbb79cae9
RobertoGine <robertogine@hotmail.com>
2025-12-04 14:53:25 -0300

    ateraçao admin models e base.html

--- financas/admin.py
+++ financas/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Transacao
-from django.contrib.auth.models import User
+from .models import Transacao, Acesso
 
-admin.site.register(User)
 
-'''@admin.register(Transacao)
+@admin.register(Transacao)
 class TransacaoAdmin(admin.ModelAdmin):
-    list_display = ('usuario','tipo','categoria','valor','data')
-    list_filter = ('tipo','categoria','data')
-'''
+    list_display = ('usuario', 'tipo', 'categoria', 'valor', 'data')
+    list_filter = ('tipo', 'categoria', 'data')
+    search_fields = ('categoria', 'descricao')
+    ordering = ('-data',)
+
+
+@admin.register(Acesso)
+class AcessoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'data_hora')
+    list_filter = ('usuario', 'data_hora')
+    search_fields = ('usuario__username',)
+    ordering = ('-data_hora',)
--- financas/models.py
+++ financas/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 from django.utils import timezone 
 
 
-
 class Acesso(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     data_hora = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.usuario.username} - {self.data_hora}"
+
 
 class Transacao(models.Model):
     TIPO_CHOICES = (
